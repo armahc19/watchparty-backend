f652bb3ddfdff53f94ffda3ba2bba6c279dfd323
scriptgud <65056977+armahc19@users.noreply.github.com>
2026-03-16 08:27:21 +0000

    Update main.go

--- main.go
+++ main.go
@@ -15,24 +15,31 @@ import (
 	"github.com/joho/godotenv"
 )
 
+
+var allowedOrigins = map[string]bool{
+	"https://watchparty-app-drab.vercel.app": true,
+	"https://watchparty.site":                true,
+	"https://www.watchparty.site":            true,
+}
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		//c.Writer.Header().Set("Access-Control-Allow-Origin", "https://watchparty-app-drab.vercel.app")
-					allowedOrigins := map[string]bool{
-			    "https://watchparty-app-drab.vercel.app": true,
-			    "https://watchparty.site": true,
-			    "https://www.watchparty.site": true,
-			}
-			
-			origin := c.Request.Header.Get("Origin")
-			
-			if allowedOrigins[origin] {
-			    c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			}
+
+		origin := c.Request.Header.Get("Origin")
+
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set(
+			"Access-Control-Allow-Headers",
+			"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+		)
+		c.Writer.Header().Set(
+			"Access-Control-Allow-Methods",
+			"POST, OPTIONS, GET, PUT, DELETE",
+		)
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
